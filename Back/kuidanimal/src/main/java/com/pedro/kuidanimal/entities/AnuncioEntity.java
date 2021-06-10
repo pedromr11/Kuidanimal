06e4a1975ce4ce637986878807d78d4b463faf19
Pedro Muñoz <pedromurivas11@gmail.com>
2021-06-10 13:20:20 +0200

    Modificacion para hacer el id autoincrement

--- Back/kuidanimal/src/main/java/com/pedro/kuidanimal/entities/AnuncioEntity.java
+++ Back/kuidanimal/src/main/java/com/pedro/kuidanimal/entities/AnuncioEntity.java
@@ -2,6 +2,8 @@ package com.pedro.kuidanimal.entities;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
@@ -10,6 +12,7 @@ import javax.persistence.Table;
 public class AnuncioEntity {
 	
 	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE)
 	@Column(name = "id_anuncio")
 	private Integer id_anuncio;
 	
